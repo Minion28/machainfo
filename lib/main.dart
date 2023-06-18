c1b489e919d824be4b0fc4f9de40252d25e40439
Minion28 <wambuakelvin14@gmail.com>
2023-06-19 00:44:20 +0300

    changed color of appdrawer and bottom navigation bar

--- lib/main.dart
+++ lib/main.dart
@@ -47,47 +47,48 @@ class _MainState extends State<MainPage> {
         data: NavigationBarThemeData(
           indicatorColor: blue,
           labelTextStyle: MaterialStateProperty.all(
-            const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: bg),),
+            const TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: dt),),
         ),
         child: NavigationBar(
           height: 60,
-          backgroundColor: dt,
+          backgroundColor: bg,
           selectedIndex: index,
           onDestinationSelected: (index) => setState(() => this.index = index),
           destinations: const [
             NavigationDestination(
-                icon: FaIcon(FontAwesomeIcons.one, color: bg,),
-                selectedIcon: FaIcon(FontAwesomeIcons.diceOne, color: bg,),
+                icon: FaIcon(FontAwesomeIcons.one, color: dt,),
+                selectedIcon: FaIcon(FontAwesomeIcons.diceOne, color: dt,),
                 label: 'One'),
             NavigationDestination(
-                icon: FaIcon(FontAwesomeIcons.two, color: bg,),
-                selectedIcon: FaIcon(FontAwesomeIcons.diceTwo, color: bg,),
+                icon: FaIcon(FontAwesomeIcons.two, color: dt,),
+                selectedIcon: FaIcon(FontAwesomeIcons.diceTwo, color: dt,),
                 label: 'Two'),
             NavigationDestination(
-                icon: FaIcon(FontAwesomeIcons.three, color: bg,),
-                selectedIcon: FaIcon(FontAwesomeIcons.diceThree, color: bg,),
+                icon: FaIcon(FontAwesomeIcons.three, color: dt,),
+                selectedIcon: FaIcon(FontAwesomeIcons.diceThree, color: dt,),
                 label: 'Three'),
             NavigationDestination(
-                icon: FaIcon(FontAwesomeIcons.four, color: bg,),
-                selectedIcon: FaIcon(FontAwesomeIcons.diceFour, color: bg,),
+                icon: FaIcon(FontAwesomeIcons.four, color: dt,),
+                selectedIcon: FaIcon(FontAwesomeIcons.diceFour, color: dt,),
                 label: 'Four'),
             NavigationDestination(
                 icon: FaIcon(FontAwesomeIcons.five,
-                color: bg,),
-                selectedIcon: FaIcon(FontAwesomeIcons.diceFive, color: bg,),
+                color: dt,),
+                selectedIcon: FaIcon(FontAwesomeIcons.diceFive, color: dt,),
                 label: 'Five'),
           ],
         ),
 
       ),
       appBar: AppBar(
+        iconTheme: const IconThemeData(color: dt),
         primary: true,
         flexibleSpace: Container(
           decoration: const BoxDecoration(
             gradient: LinearGradient(
               begin: Alignment.topLeft,
               end: Alignment.topRight,
-              colors: <Color>[dt, dt],
+              colors: <Color>[bg, bg],
             )
           ),
         ),
